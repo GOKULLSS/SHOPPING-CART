a3a681e5f762dbcd4500b7cf3cfad14c31a1c4e7
Gokul <gokulofficial556@gmail.com>
2025-07-27 14:12:15 +0530

    Add user and admin routes for product display

---
+++ routes/admin.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var productHelper=require('../helpers/product-helpers');
+const productHelpers = require('../helpers/product-helpers');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+   productHelpers.getAllProducts().then((products)=>{
+    console.log(products)
+            res.render('admin/view-products',{admin:true,products});
+   })
+ 
+});
+router.get('/add-product',function(req,res){
+  res.render('admin/add-product')
+})
+router.post('/add-product',(req,res)=>{
+  console.log(req.body);
+  console.log(req.files.Image);
+  productHelper.addProduct(req.body,(insertedId)=>{
+    let image=req.files.Image
+    console.log(insertedId);
+    image.mv('./public/product-images/'+insertedId+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-product")
+      }else{
+        console.log(err);
+      }
+    })
+    
+  })
+
+})
+
+module.exports = router;

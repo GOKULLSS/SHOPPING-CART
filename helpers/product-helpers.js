5dfaae25afa7f1693f1a5c5620fb7d8f0ff24d7e
Gokul <gokulofficial556@gmail.com>
2025-08-02 15:43:54 +0530

    Delete button added

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,7 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
+const { ObjectId } = require('mongodb');
+
 module.exports={
 
 
@@ -16,5 +18,17 @@ module.exports={
             let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
             resolve(products)
         })
+    },
+    deleteProduct:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            console.log(proId);
+            const objId = new ObjectId(proId);
+            console.log( objId);
+            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({_id:objId}).then((response)=>{
+                //console.log(response);
+                resolve(response)
+            })
+        })
     }
+
 }

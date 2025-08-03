694e4810baee081169ad04e5e420f2762c7b9883
Gokul <gokulofficial556@gmail.com>
2025-08-03 12:59:10 +0530

    Edit button added

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
 const { ObjectId } = require('mongodb');
+const { response } = require('express');
 
 module.exports={
 
@@ -28,6 +29,28 @@ module.exports={
                 //console.log(response);
                 resolve(response)
             })
+        })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:new ObjectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise ((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:new ObjectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Category:proDetails.Category
+                }
+            }).then((response)=>{
+                resolve()
+            })
+
         })
     }
 
